f903df03ef1325b24c601da94208f37deb853efa
Artem Barkalov <artembark@gmail.com>
2022-04-11 00:53:07 +0300

    sw mod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/screens/input_page.dart';
+import 'package:vk_bridge/vk_bridge.dart';
 
-void main() => runApp(BMICalculator());
+Future<void> main() async {
+  await VKBridge.instance.init();
+
+  runApp(BMICalculator());
+}
 
 class BMICalculator extends StatelessWidget {
   @override
